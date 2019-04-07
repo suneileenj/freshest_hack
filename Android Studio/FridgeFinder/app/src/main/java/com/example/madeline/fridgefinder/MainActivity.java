f18a7038ac3bb49a682e4d6194b6c68a1b835d48
Madeline <mjtjoa@ucsd.edu>
2019-04-07 00:01:48 -0700

    hullo

--- Android Studio/FridgeFinder/app/src/main/java/com/example/madeline/fridgefinder/MainActivity.java
+++ Android Studio/FridgeFinder/app/src/main/java/com/example/madeline/fridgefinder/MainActivity.java
@@ -1,32 +1,15 @@
 package com.example.madeline.fridgefinder;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
 import android.widget.TextView;
-import android.app.Activity;
-import android.content.Intent;
-import android.os.Bundle;
-import android.provider.MediaStore;
-import android.view.Menu;
-import android.view.View;
-import android.view.View.OnClickListener;
-import android.widget.Button;
-
-
 
 public class MainActivity extends AppCompatActivity {
 
     private TextView mTextMessage;
-
-    Button btn;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -37,20 +20,15 @@ public class MainActivity extends AppCompatActivity {
                 case R.id.navigation_home:
                     mTextMessage.setText(R.string.title_home);
                     return true;
-
                 case R.id.navigation_dashboard:
                     mTextMessage.setText(R.string.title_dashboard);
                     return true;
-                case R.id.navigation_camera:
-
-
+                case R.id.navigation_notifications:
                     mTextMessage.setText(R.string.title_notifications);
                     return true;
             }
             return false;
         }
-
-
     };
 
     @Override
@@ -61,24 +39,6 @@ public class MainActivity extends AppCompatActivity {
         mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
-
-        btn =(Button)findViewById(R.id.button1);
-        btn.setOnClickListener(new OnClickListener() {
-
-            public void onClick(View v) {
-                // TODO Auto-generated method stub
-                Intent i=new Intent(MediaStore.ACTION_VIDEO_CAPTURE);
-                startActivity(i);
-            }
-        });
-
-
     }
-//    public void sendMessage(View view){
-//        Intent startNewActivity = new Intent(this, ListActivity.class);
-//        EditText theEditText = (EditText) findViewById(R.id.edit_message);
-//        String message = theEditText.getText().toString();
-//        startNewActivity.putExtra(EXTRA_MESSAGE, message);
-//        startActivity(startNewActivity);
-//    }
+
 }
