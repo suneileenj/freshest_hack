930c29be4204d25cbc6328f8e7e6898c97ab15a2
Madeline <mjtjoa@ucsd.edu>
2019-04-07 00:57:23 -0700

    pppp

--- Android_Studio/app/src/main/java/com/example/madeline/fridgefinder/MainActivity.java
+++ Android_Studio/app/src/main/java/com/example/madeline/fridgefinder/MainActivity.java
@@ -1,39 +1,46 @@
 package com.example.madeline.fridgefinder;
 
+import android.Manifest;
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.drawable.Drawable;
 import android.net.Uri;
+import android.os.Build;
+import android.os.Environment;
 import android.provider.MediaStore;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 
 import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
     Button button;
     ImageView imageView;
+    String pathToFile;
     static final int CAM_REQUEST = 1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
         button = (Button) findViewById(R.id.button);
+        if(Build.VERSION.SDK_INT >=23){
+            requestPermissions(new String[]{Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE}, 2);
+        }
         imageView = (ImageView) findViewById(R.id.image_view);
-
-
         button.setOnClickListener(new View.OnClickListener(){
+            @Override
             public void onClick(View v){
-                Intent camera_intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                File file = getFile();
-                camera_intent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(file));
-
-                startActivityForResult(camera_intent,CAM_REQUEST);
+                dispatchPictureTakerAction();
 
             }
 
@@ -56,11 +63,52 @@ public class MainActivity extends Activity {
 
     protected void onActivityResult(int requestCode, int resultCode, Intent data){
 
-        String path = "sdcard/camera_app/cam_image.jpg";
-        imageView.setImageDrawable(Drawable.createFromPath(path));
+       super.onActivityResult(requestCode,resultCode,data);
+       if(resultCode == RESULT_OK){
+           if(requestCode == 1){
+               Bitmap bitmap = BitmapFactory.decodeFile(pathToFile);
+               imageView.setImageBitmap(bitmap);
 
+           }
+       }
 
     }
 
+    private void dispatchPictureTakerAction(){
+        Intent takePic = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if(takePic.resolveActivity(getPackageManager())!=null){
+            File photoFile = null;
+            photoFile = createPhotoFile();
+            if(photoFile!=null){
+
+                String pathToFile = photoFile.getAbsolutePath();
+                Uri photoURI = FileProvider.getUriForFile(MainActivity.this, "com.thecodecity.cameraandroid.fileprovider", photoFile);
+                takePic.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(takePic,1);
+            }
+
+//            try{
+//
+//                photoFile = createPhotoFile();
+//            }
+//            catch(Exception){
+
+
+
+        }
+    }
+
+    private File createPhotoFile() {
+        String name = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = null;
+        try {
+             image = File.createTempFile(name, ".jpg", storageDir);
+        }
+        catch(IOException e){
+            Log.d("mylog", "Excep : " + e.toString());
+        }
+        return image;
+    }
 
 }
